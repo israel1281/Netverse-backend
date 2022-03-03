087ef737953d329fb2bc1e55832a22fb50e60829
Israel David <77063876+israel1281@users.noreply.github.com>
2022-03-03 00:44:51 +0000

    init commit

--- server.js
+++ server.js
@@ -9,6 +9,17 @@ app.use(cookieParser())
 
 const http = require('http').createServer(app)
 
+const URI = process.env.MONGODB_URL
+mongoose.connect(URI, {
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}, err => {
+  if(err) throw err;
+  console.log('Connected to mongodb')
+})
+
 const port = process.env.PORT || 3000
 http.listen(port, () => {
   console.log('Server is running on port', port)
