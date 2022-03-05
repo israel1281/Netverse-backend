5670efb06c27561080714c37d5eced354434ba4f
Israel David <77063876+israel1281@users.noreply.github.com>
2022-03-05 07:31:16 +0000

    init commit

--- server.js
+++ server.js
@@ -44,12 +44,6 @@ mongoose.connect(URI, {
     console.log('Connected to mongodb')
 })
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/build'))
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
-    })
-}
 
 
 const port = process.env.PORT || 5000
