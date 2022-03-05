996d6e2044c67c5b8b57a94c2df6ecc9f0ea9875
Israel David <77063876+israel1281@users.noreply.github.com>
2022-03-05 06:05:11 +0000

    init commit

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -7,6 +7,6 @@ router.post('/login', authCtrl.login)
 
 router.post('/logout', authCtrl.logout)
 
-router.post('refresh_token', authCtrl.generateAccessToken)
+router.post('/refresh_token', authCtrl.generateAccessToken)
 
 module.exports = router
