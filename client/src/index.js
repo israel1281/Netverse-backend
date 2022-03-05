fa34aae176352cdc0f5579e4dc08c14fa070756d
Israel David <77063876+israel1281@users.noreply.github.com>
2022-03-05 11:00:11 +0000

    init commit

--- client/src/index.js
+++ client/src/index.js
@@ -3,13 +3,10 @@ import "./index.css"
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import DataProvider from './redux/store'
 
 ReactDOM.render(
   <React.StrictMode>
-    <DataProvider>
       <App />
-    </DataProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
