d65240ae485825e150e9186ec13a87508f7ca920
Israel David <77063876+israel1281@users.noreply.github.com>
2022-03-09 09:16:10 +0000

    init commit

--- client/src/components/alert/Alert.js
+++ client/src/components/alert/Alert.js
@@ -2,7 +2,6 @@ import React from 'react'
 
 
 import Loading from './Loading'
-// import Toast from './Toast'
 
 const Notify = () => {
  
@@ -10,8 +9,6 @@ const Notify = () => {
     return (
         <div>
          <Loading /> 
-   /*      <Toast msg="error" bgColor="bg-danger" />
-         <Toast msg="success" bgColor="bg-success" /> */
         </div>
     )
 }
