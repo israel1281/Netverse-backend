3274dba3c79ecec25447be788b89ef7fd68e8a08
israel1281 <kidoski1@gmail.com>
2022-03-09 12:58:26 +0000

    init commit

--- client/src/components/alert/Alert.js
+++ client/src/components/alert/Alert.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { GlOBALTYPES } from '../../redux/actions/globalTypes'
+import { GLOBALTYPES } from '../../redux/actions/globalTypes'
 
 import Loading from './Loading'
 import Toast from './Toast'
