7f27db4b9a6efac94007207810d9b9fb93e31f0b
Israel David <77063876+israel1281@users.noreply.github.com>
2022-03-28 02:55:08 +0100

    Init commit

--- client/src/components/header/Menu.js
+++ client/src/components/header/Menu.js
@@ -4,8 +4,6 @@ import { Link, useLocation } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
 import { logout } from '../../redux/actions/authAction'
 import { GLOBALTYPES } from '../../redux/actions/globalTypes'
-import Avatar from '../Avatar'
-import NotifyModal from '../NotifyModal'
 
 const Menu = ({ avatar, Icon , title, onClick }) => {
 
