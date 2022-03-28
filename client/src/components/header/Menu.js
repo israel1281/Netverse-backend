b39c10d1a0c9d9988204a7b28cf79f078ba3f4fc
Israel David <77063876+israel1281@users.noreply.github.com>
2022-03-28 02:52:39 +0100

    Init commit

---
+++ client/src/components/header/Menu.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import "./Menu.css"
+import { Link, useLocation } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import { logout } from '../../redux/actions/authAction'
+import { GLOBALTYPES } from '../../redux/actions/globalTypes'
+import Avatar from '../Avatar'
+import NotifyModal from '../NotifyModal'
+
+const Menu = ({ avatar, Icon , title, onClick }) => {
+
+    const { auth, theme, notify } = useSelector(state => state)
+    const dispatch = useDispatch()
+    const { pathname } = useLocation()
+
+    const isActive = (pn) => {
+        if(pn === pathname) return 'active'
+    }
+
+return (
+<div className="headerOption">
+{Icon && <Icon className="headerOption_icon" />}
+<h3 className="headerOption_title">{title}</h3>
+</div>
+)
+}
+
+export default Menu
