56c964e30d08568cd88cd7847dfa788aa3837048
Israel David <77063876+israel1281@users.noreply.github.com>
2022-03-28 02:50:50 +0100

    Init commit

---
+++ client/src/components/header/Search.js
@@ -0,0 +1,74 @@
+import React, { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { getDataAPI } from '../../utils/fetchData'
+import { GLOBALTYPES } from '../../redux/actions/globalTypes'
+import UserCard from '../UserCard'
+import SearchIcon from '@material-ui/icons/Search';
+import LoadIcon from '../../images/loading.gif'
+import "./Search.css"
+
+
+const Search = () => {
+    const [search, setSearch] = useState('')
+    const [users, setUsers] = useState([])
+
+    const { auth } = useSelector(state => state)
+    const dispatch = useDispatch()
+    const [load, setLoad] = useState(false)
+
+
+    const handleSearch = async (e) => {
+        e.preventDefault()
+        if(!search) return;
+
+        try {
+            setLoad(true)
+            const res = await getDataAPI(`search?username=${search}`, auth.token)
+            setUsers(res.data.users)
+            setLoad(false)
+        } catch (err) {
+            dispatch({
+                type: GLOBALTYPES.ALERT, payload: {error: err.response.data.msg}
+            })
+        }
+    }
+
+    const handleClose = () => {
+        setSearch('')
+        setUsers([])
+    }
+
+    return (
+        <form  onSubmit={handleSearch} className="header_search">
+              
+            <SearchIcon style={{ color: 'black'}} />
+
+            <input type="text" placeholder="Search" name="search" value={search} id="search" title="Enter to Search"
+            onChange={e => setSearch(e.target.value.toLowerCase().replace(/ /g, ''))} />
+
+            <div className="close_search" onClick={handleClose}
+            style={{opacity: users.length === 0 ? 0 : 1}} >
+                &times;
+            </div>
+
+            <button type="submit" style={{display: 'none'}}>Search</button>
+
+            { load && <img className="loading" src={LoadIcon} alt="loading"  /> }
+
+            <div className="users">
+                {
+                    search && users.map(user => (
+                        <UserCard 
+                        key={user._id} 
+                        user={user} 
+                        border="border"
+                        handleClose={handleClose} 
+                        />
+                    ))
+                }
+            </div>
+        </form>
+    )
+}
+
+export default Search
